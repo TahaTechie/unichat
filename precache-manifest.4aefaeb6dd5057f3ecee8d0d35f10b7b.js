01a9c74b04ad6a7a9eecb3d805fef039126a343f
Taha Amir <tahaamir550@gmail.com>
2022-03-31 14:53:59 -0700

    Updates

---
+++ precache-manifest.4aefaeb6dd5057f3ecee8d0d35f10b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d306141b5f6ac95a617458b1afdd2c19",
+    "url": "/unichat/index.html"
+  },
+  {
+    "revision": "14a30ddb8a08011feb46",
+    "url": "/unichat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "170c609e793186b7c0f6",
+    "url": "/unichat/static/js/2.6cbf288a.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/unichat/static/js/2.6cbf288a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a30ddb8a08011feb46",
+    "url": "/unichat/static/js/main.5c69e844.chunk.js"
+  },
+  {
+    "revision": "d81067fa5f1d98a40195",
+    "url": "/unichat/static/js/runtime-main.014caeaa.js"
+  }
+]);
